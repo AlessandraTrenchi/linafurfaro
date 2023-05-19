416682239e833300aa191a4b22f27c42b3cb26b0
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-05-19 15:51:09 +0200

    index

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './Navbar';
 
 
 function App() {
-  const proPic = <img src="https://pbs.twimg.com/profile_images/768896426038423552/nGiJamXP_400x400.jpg" alt="Profile Picture"></img>;
+  const proPic = <img src="https://pbs.twimg.com/profile_images/768896426038423552/nGiJamXP_400x400.jpg" alt="Profile Picture" className='imm'></img>;
   return (
     <>
     <Navbar/>
--- src/Navbar.js
+++ src/Navbar.js
@@ -7,49 +7,99 @@ import './Navbar.css';
 function Navbar() {
     const author = "Lina Furfaro";
     const [isOpen, setIsOpen] = useState(false);
-  
+    const [searchTerm, setSearchTerm] = useState('');
     const toggleDropdown = () => {
       setIsOpen(!isOpen);
     };
-  
+    const handleSearch = (event) => {
+        const searchTerm = event.target.value;
+        setSearchTerm(searchTerm);
+      };
+      // Define your search logic here
+    const searchElements = () => {
+    // Perform search logic based on the search term (searchTerm)
+    // Return the search results as an array of elements or an appropriate data structure
+    // For simplicity, let's assume you have an array of nav links
+    const navLinks = [
+      { id: 1, title: 'Home', url: './index.html' },
+      { id: 2, title: 'Libri', url: './libri.html' },
+      { id: 2, title: 'Poesie', url: './poesie.html' },
+      { id: 2, title: 'Articoli', url: './articoli.html' },
+      { id: 2, title: 'Copioni', url: './copioni.html' },
+      { id: 3, title: 'Incontri', url: './incontri.html' },
+      { id: 4, title: 'Teatro', url: './teatro.html' },
+      { id: 5, title: 'Recensisco', url: './recensisco.html' },
+      { id: 6, title: 'Scrivono', url: './scrivono.html' },
+      { id: 7, title: 'Tempi', url: './tempinuovi.html' },
+      // Add more nav links
+    ];
+    // Filter the nav links based on the search term
+    const filteredNavLinks = navLinks.filter((link) =>
+      link.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    return filteredNavLinks;
+};
     return (
       <nav className='Navbar'>
         <ul className='menu'>
           <ul className='dropdown-menu'>
-            <li className="in">{author}</li>
-            <button onClick={toggleDropdown}>HOME</button>
+            
+            <li className="inx">{author}</li>
+            <li>
+                <button onClick={toggleDropdown}>HOME</button>
+                {isOpen && (
+                <ul className='dropdown'>
+                    <li a href="./eventi.html">EVENTI</li>
+                    <li a href="./eventipassati.html">EVENTI PASSATI</li>
+                    <li a href="./riconoscimenti.html">RICONOSCIMENTI</li>
+                    </ul>
+                    )}
+             </li>
+             <li>
+             <button onClick={toggleDropdown}>PUBBLICAZIONI</button>
             {isOpen && (
               <ul className='dropdown'>
-                <li>EVENTI</li>
-                <li>EVENTI PASSATI</li>
-                <li>RICONOSCIMENTI</li>
+                <li className='navs'> <a href="./libri.html">LIBRI</a></li>
+                <li> <a href="./poesie.html">POESIE</a></li>
+                <li> <a href="./articoli.html">ARTICOLI</a></li>
+                <li><a href="./copioni.html" >COPIONI TEATRALI</a></li>
               </ul>
-            )}
-            <button onClick={toggleDropdown}>PUBBLICAZIONI</button>
-            {isOpen && (
-              <ul className='dropdown'>
-                <li>LIBRI</li>
-                <li>POESIE</li>
-                <li>ARTICOLI</li>
-                <li>COPIONI TEATRALI</li>
-              </ul>
-            )}
-            <li className='in'>L'AUTRICE</li>
-            <li className="in">INCONTRI CON L'AUTRICE</li>
-            <li className="in">TEATRO</li>
-            <button onClick={toggleDropdown}>DIDATTICA</button>
-            {isOpen && (
-              <ul className='dropdown'>
-                <li>PROBLEMI</li>
-                <li>DISEGNI DEI BAMBINI</li>
-              </ul>
-            )}
-            <li className="in">RECENSISCO</li>
-            <li className="in">SCRIVONO DI ME</li>
-            <li className="in">TEMPI NUOVI</li>
-            <li className="in">CONTATTI</li>
+              )}
+             </li>
+            <li><a href="./incontri.html">INCONTRI CON L'AUTRICE</a></li>
+            <li className="in"><a href="">TEATRO</a></li>
+            <li>
+                <button onClick={toggleDropdown}>DIDATTICA</button>
+                {isOpen && (
+                <ul className='dropdown'>
+                    <li><a href="./problemi.html">PROBLEMI</a></li>
+                    <li><a href="./disegni.html">DISEGNI DEI BAMBINI</a></li>
+                    </ul>
+                    )}
+            </li>
+            
+            <li className="in"><a href="./recensisco.html">RECENSISCO</a></li>
+            <li className="in"><a href="./scrivono.html">SCRIVONO DI ME</a></li>
+            <li className="in"><a href="./tempinuovi.html">TEMPI NUOVI</a></li>
+            <li className="in"><a href="mailto:linafurfaro@gmail.com">CONTATTI</a></li>
+            <li className="search">
+          <input type="text" placeholder="Cerca..." value={searchTerm} onChange={handleSearch}/>
+          {/* Render search results */}
+          {searchTerm && (
+            <li className="search-results">
+              <ul>
+                {searchElements().map((result) => (
+                  <li key={result.id}>
+                    <a href={result.url}>{result.title}</a>
+                  </li>
+                ))}
+              </ul> 
+            </li>
+          )}
+        </li>
           </ul>
         </ul>
+       
       </nav>
     );
   }
